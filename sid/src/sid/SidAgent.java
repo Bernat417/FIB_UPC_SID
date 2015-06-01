2c976cf6c73fbf65032d91b7f46e791498cb7e21
carlosep93 <carlos.e.p93@gmail.com>
2015-06-01 21:53:54 +0200

    Agente que consulta la ontologia. Funciona

--- sid/src/sid/SidAgent.java
+++ sid/src/sid/SidAgent.java
@@ -14,10 +14,8 @@ import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.shared.JenaException;
 import jade.core.Agent;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 /**
  *
@@ -27,35 +25,34 @@ public class SidAgent extends Agent {
    
     
     protected void setup() {
-        String source = "http://www.semanticweb.org/adriàabella/ontologies/2015/4/untitled-ontology-7";
-        String ns = source + "#";
-        File file = new File ("/home/carlos/Documentos/sid/projecto/project.owl");
-        FileInputStream fis = null;
+       
+        
+        OntModel model1 = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
         
         try {
-            fis = new FileInputStream(file);
-        } 
-        catch (IOException e) {
-			e.printStackTrace();
+            model1.read("file:/home/carlos/Documentos/sid/projecto/project.owl", "RDF/XML");
         }
-        OntModel model1 = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM);
+        catch (JenaException je) {        
+
+           System.out.println("ERROR");
+
+           je.printStackTrace();
+
+           System.exit(0);
+
+        }
         
-        // read the RDF/XML file
-        model1.read( fis, "RDF/XML" );
         
         //Create a new query
-        String queryString = 
-        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-        "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
-        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" +
-        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+         String queryString = 
         "PREFIX :<http://www.semanticweb.org/adriàabella/ontologies/2015/4/untitled-ontology-7#>" +
         "SELECT ?nombre ?apellidos ?dolencia_nombre" +
         "WHERE { ?paciente a :Paciente." +
         "?paciente :Nombre_persona ?nombre." +
         "?paciente :Apellidos ?apellidos." +
         "?paciente :Padece ?dolencia." +
-        "?dolencia :Nombre_dolencia ?dolencia_nombre.";
+        "?dolencia :Nombre_dolencia ?dolencia_nombre.}\n"+
+        "";
         
          Query query = QueryFactory.create(queryString);
         
@@ -66,7 +63,9 @@ public class SidAgent extends Agent {
           // Output query results    
          ResultSetFormatter.out(System.out, results, query);
 
-         qe.close();
+         qe.close(); 
+        
+        System.out.println("Agente de SID");
 
                 
                 

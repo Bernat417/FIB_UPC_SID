30dac170d06cdaa7694b1f3a666d05681f03bc99
carlosep93 <carlos.e.p93@gmail.com>
2015-06-21 08:33:51 +0200

    login doctor

--- Agents/DoctorAgent.java
+++ Agents/DoctorAgent.java
@@ -8,10 +8,12 @@ import java.util.Scanner;
 
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.QuerySolutionMap;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
@@ -31,8 +33,55 @@ public class DoctorAgent extends Agent {
      
     Scanner keyboard = new Scanner(System.in);
     String NS = "http://www.semanticweb.org/adriàabella/ontologies/2015/4/untitled-ontology-7#";
+    String username;
+    String password;
+    String dniPersona;
+    OntModel model1;
     
-    private void creaAccion(OntModel model1) {    
+        public void login() {
+        boolean correct = false;
+        while (!correct) {
+
+            System.out.println("Introduce tu nombre de usuario:");
+            username = keyboard.nextLine();
+            System.out.println("Introduce tu contraseña: ");
+            password = keyboard.nextLine();
+
+            String QueryString = 
+            "PREFIX :<http://www.semanticweb.org/adriàabella/ontologies/2015/4/untitled-ontology-7#>" +
+            "SELECT ?dni\n" +
+            "WHERE {\n" +        
+            "?login a :LogIn.\n" +
+            "?login :Username ?user.\n" +
+            "?login :Password ?pass.\n" +
+            "?login :Identifica ?persona." + 
+            "?persona :Dni ?dni." +        
+            "FILTER regex(?user, ?u). \n" +
+            "FILTER regex(?pass, ?p). \n" +
+            "}\n"+ "";   
+
+            ParameterizedSparqlString str = new ParameterizedSparqlString(QueryString);
+            str.setLiteral("u", username.toString());
+            str.setLiteral("p",password.toString());
+
+            Query query = QueryFactory.create(str.toString());
+            QueryExecution qe2 = QueryExecutionFactory.create(query, model1);
+            ResultSet results =  qe2.execSelect();
+            if (results.hasNext()) {
+                QuerySolution row = results.nextSolution();
+                dniPersona = row.getLiteral("dni").getString();
+                System.out.println(dniPersona);
+                correct = true;
+                System.out.println("Usuario correcto");
+            }    
+            else
+                System.out.println("Usuario o contraseña incorrectos");
+            qe2.close();
+
+        }
+    }
+    
+    private void creaAccion() {    
         boolean correcta = false;
         String nombreAccion = "";
         String descripcionAccion  = "";
@@ -42,7 +91,7 @@ public class DoctorAgent extends Agent {
             System.out.println("Introduce la descripcion de la accion");
             descripcionAccion = keyboard.nextLine();
             
-            Individual t = getIndividual(model1,nombreAccion);
+            Individual t = getIndividual(nombreAccion);
             if (t == null)
                 correcta = true;
             else 
@@ -72,7 +121,7 @@ public class DoctorAgent extends Agent {
            System.out.println("Introduce el nombre del tratamiento seleccionado");
            nombreTratamiento = keyboard.nextLine();
 
-            Individual t = getIndividual(model1,nombreTratamiento);
+            Individual t = getIndividual(nombreTratamiento);
             
             if (t != null) {
                 correcta = true;
@@ -91,8 +140,77 @@ public class DoctorAgent extends Agent {
         }
        
     }
+    
+    private void creaTratamiento() {    
+        boolean correcta = false;
+//        String nombreAccion = "";
+        String nombreTratamiento = "";
+//        String descripcionAccion  = "";
+        while (!correcta) {
+            System.out.println("Introduce el nombre del tratamiento para crear: ");
+            nombreTratamiento = keyboard.nextLine();       
+            
+            Individual t = getIndividual(nombreTratamiento);
+            if (t == null)
+                correcta = true;
+            else 
+                System.out.println("Ya exista el tratamiento");            
+        }   
+        
+        System.out.println("Tratamientos disponibles en el sistema");
+        
+        String queryString = 
+            "PREFIX :<http://www.semanticweb.org/adriàabella/ontologies/2015/4/untitled-ontology-7#>" +
+            "SELECT * \n" +
+            "WHERE { ?accion a :Acción." +
+            "}\n"+
+            "";
+
+        Query query2 = QueryFactory.create(queryString);
+        QueryExecution qe2 = QueryExecutionFactory.create(query2, model1);
+        ResultSet results =  qe2.execSelect();
+        ResultSetFormatter.out(System.out, results, query2);
+        qe2.close();
+        Individual T;
+
+        correcta = false;
+        String nombreAccion = "";
+        while(!correcta) {
+           System.out.println("Introduce el nombre de la acción seleccionada");
+           nombreAccion = keyboard.nextLine();
+
+            Individual t = getIndividual(nombreAccion);
+            
+            if (t != null) {
+                correcta = true;
+                
+//                OntClass att = model1.getOntClass(NS + "Acción");
+//                Individual I1 = model1.createIndividual(NS + nombreAccion, att);
+//                Property nombre = model1.createProperty(NS +"Nombre_accion");
+                
+//                Property descripcion = model1.createProperty(NS +"Descripcion");
+//                Property pertenece = model1.createProperty(NS +"Pertenece_a_tratamiento");
+//                model1.add(I1, nombre, nombreAccion);
+//                model1.add(I1,descripcion,descripcionAccion);
+//                model1.add(I1,pertenece,t);
+                
+               
+                OntClass att = model1.getOntClass(NS + "Tratamiento");
+                Individual I1 = model1.createIndividual(NS + nombreTratamiento, att);
+                Property nombre = model1.createProperty(NS +"Nombre_tratamiento");
+                
+                Property pertenece = model1.createProperty(NS +"Pertenece_a_tratamiento");
+                Property contiene = model1.createProperty(NS +"Contiene_accion");
+                model1.add(I1, nombre, nombreTratamiento);
+                model1.add(I1, contiene, t);
+                
+            }
+            else
+                System.out.println("Nombre de acción incorrecto");                  
+        }  
+    }
    
-    public Individual getIndividual(OntModel model1, String nombreTratamiento)
+    public Individual getIndividual(String nombre)
     {
    
         Iterator<OntClass> classesIt = model1.listNamedClasses();
@@ -105,7 +223,7 @@ public class DoctorAgent extends Agent {
             for (Iterator i = model1.listIndividuals(Tratamiento); i.hasNext(); )
             {
                 trat = (Individual) i.next();
-                if (trat.getLocalName().equals(nombreTratamiento))
+                if (trat.getLocalName().equals(nombre))
                     return trat;
             }
         }
@@ -115,7 +233,7 @@ public class DoctorAgent extends Agent {
     
     protected void setup() {
                
-        OntModel model1 = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+        model1 = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
         
         try {
             model1.read("file:/home/carlos/Documentos/sid/proyecto/projectRDF.owl", "RDF/XML");
@@ -126,11 +244,17 @@ public class DoctorAgent extends Agent {
            System.exit(0);
         }
         
+         login();
+        
         System.out.println("Selecciona una accion a realizar:");
         System.out.println("1. Crear una accion");
+        System.out.println("2. Crear un tratamiento");
         String opcion = keyboard.nextLine();
+        
         if (opcion.equals("1")) 
-           creaAccion(model1);
+           creaAccion();
+        else if(opcion.equals("2"))
+           creaTratamiento();
                 
         //Create a new query
              String queryString = 
